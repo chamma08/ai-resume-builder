906e247e187875ac1be41e8ab85cb70e809e8c61
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-10-30 19:58:13 +0530

    created model schema for resume

---
+++ server/models/Resume.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const ResumeSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, default: "Untitled Resume" },
+    public: { type: Boolean, default: false },
+    professional_summary: { type: String, default: "" },
+    skills: [{ type: String }],
+    personal_info: {
+      full_name: { type: String, default: "" },
+      email: { type: String, default: "" },
+      phone: { type: String, default: "" },
+      location: { type: String, default: "" },
+      linkedin: { type: String, default: "" },
+      website: { type: String, default: "" },
+      proffession: { type: String, default: "" },
+      image: { type: String, default: "" },
+    },
+    experience: [
+      {
+        company: { type: String, default: "" },
+        position: { type: String, default: "" },
+        start_date: { type: String, default: "" },
+        end_date: { type: String, default: "" },
+        description: { type: String, default: "" },
+        is_current: { type: Boolean, default: false },
+      },
+    ],
+    education: [
+      {
+        institution: { type: String, default: "" },
+        degree: { type: String, default: "" },
+        field: { type: String, default: "" },
+        graduation_date: { type: String, default: "" },
+        gpa: { type: String, default: "" },
+      },
+    ],
+    templates: { type: String, default: "Classic" },
+    accent_color: { type: String, default: "#3B82F6" },
+    project: [
+      {
+        name: { type: String, default: "" },
+        type: { type: String, default: "" },
+        description: { type: String, default: "" },
+      },
+    ],
+  },
+  { timestamps: true, minimize: false }
+);
+
+const Resume = mongoose.model("Resume", ResumeSchema);
+
+export default Resume;
