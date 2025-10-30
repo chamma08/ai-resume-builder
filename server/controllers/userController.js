3e13f054dea4c7a07395f01f4f11975a23868410
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-10-30 12:49:04 +0530

    created getuser logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,3 +77,23 @@ export const signIn = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const getUserById = async (req, res) => {
+    try {
+        const userId = userId
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json({
+            message: "User retrieved successfully",
+            user: {
+                ...user.toObject(),
+                password: undefined,
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
