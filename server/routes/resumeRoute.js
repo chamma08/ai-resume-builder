38a414f7345afda77fd19c6b63301403a49d741b
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-10-31 08:47:42 +0530

    Created routes for resumes

---
+++ server/routes/resumeRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createResume,
+  deleteResume,
+  getPublicResumeById,
+  getResumeById,
+  updateResume,
+} from "../controllers/resumeController.js";
+import { protect } from "../middlewares/authMiddleware.s";
+import upload from "../configs/multer.js";
+
+const router = express.Router();
+
+router.post("/create-resume", protect, createResume);
+router.put("/update-resume", upload.single("image"), protect, updateResume);
+router.delete("/delete-resume/:resumeId", protect, deleteResume);
+router.post("/get-resume/:resumeId", protect, getResumeById);
+router.get("/get-public-resume/:resumeId", getPublicResumeById);
+
+export default router;
