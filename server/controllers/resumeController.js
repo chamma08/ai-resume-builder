db602fd9aec1da8ff865cd2d9af4b631d2e5aa63
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-10-31 08:35:37 +0530

    Created update resume function

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,105 @@
+import Resume from "../models/Resume.js";
+
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { title } = req.body;
+
+    const newResume = await Resume.create({
+      userId,
+      title,
+    });
+
+    return res.status(201).json({
+      message: "Resume created successfully",
+      resume: newResume,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOneAndDelete({ _id: resumeId, userId });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getResumeById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ _id: resumeId, userId });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    resume.__v = undefined;
+    resume.createdAt = undefined;
+    resume.updatedAt = undefined;
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getPublicResumeById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ _id: resumeId, public: true });
+
+    if (!resume) {
+      return res
+        .status(404)
+        .json({ message: "Resume not found or is not public" });
+    }
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId, resumeData, removeBackground } = req.body;
+    const image = req.file;
+
+    let resumeDataCopy = JSON.parse(resumeData);
+
+    const resume = await Resume.findByIdAndUpdate(
+      {
+        userId,
+        _id: resumeId,
+      },
+      resumeDataCopy,
+      { new: true }
+    );
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Resume Saved successfully", resume });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
