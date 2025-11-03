a2f5231b1ff392dfab971f3527d4b054a8a504b6
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-11-03 09:16:36 +0530

    Preview page updated

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,20 +1,25 @@
 import { useParams } from "react-router-dom";
-import { dummyResumeData } from "../assets/assets.js";
 import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen.jsx";
 import ResumePreview from "../components/ResumeBuilderSections/ResumePreview.jsx";
 import NotFound from "../components/NotFound.jsx";
+import API from "../configs/api.js";
 
 export default function Preview() {
   const { resumeId } = useParams();
   const [resumeData, setResumeData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const loadResume = async () => {
-    setResumeData(
-      dummyResumeData.find((resume) => resume._id === resumeId) || null
-    );
-    document.title = "Preview - AI Resume Builder";
-    setIsLoading(false);
+    try {
+      const { data } = await API.get(`/api/resumes/get-public-resume/${resumeId}`);
+      document.title = `Preview - ${data.resume.title}`;
+      setResumeData(data.resume);
+    } catch (error) {
+      console.error("Error fetching resume:", error.message);
+    }
+    finally{
+      setIsLoading(false);
+    }
   };
   useEffect(() => {
     loadResume();
