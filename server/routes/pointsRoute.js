23cb235c2c53776781366d39cf9d25690c797543
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-11-09 09:27:49 +0530

    Created routes

--- server/routes/pointsRoute.js
+++ server/routes/pointsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protect } from '../middlewares/authMiddleware.js';
-import { awardPoints, generateReferralCode, getActivityHistory, getLeaderboard, getUserPoints, recordSocialFollow } from '../controllers/pointsController.js';
+import { awardPoints, checkBalance, deductPoints, generateReferralCode, getActivityHistory, getLeaderboard, getTemplateDownloadCost, getUserPoints, recordSocialFollow } from '../controllers/pointsController.js';
 
 const router = express.Router();
 
@@ -23,5 +23,9 @@ router.post('/follow', recordSocialFollow);
 
 // Generate/get referral code
 router.get('/referral-code', generateReferralCode);
+
+router.post('/deduct', deductPoints);
+router.get('/check-balance', checkBalance);
+router.get('/template-cost/:templateType', getTemplateDownloadCost);
 
 export default router;
